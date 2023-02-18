304c7d5885d6b8c6df55ef48552113489ea18f23
Tran Phan <tranphan2910@gmail.com>
2023-02-18 12:58:21 +0700

    first commit

---
+++ zuizui.py
@@ -0,0 +1,11 @@
+a = ['ho','Tuan','thanh']
+def check_name(name): 
+    flag = True         
+    for i in name:
+        if i == i.capitalize():
+            flag = True
+        else:
+            flag = False
+    return flag
+
+print(check_name(a))
